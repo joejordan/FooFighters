78953c003c5f281a6153cc82d5aa1733da752714
Joe Jordan <dev@ignitesoft.com>
2022-09-05 23:42:15 -0400

    FooFighters setup

---
+++ src/FooFightersBot.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract FooFightersBot {
+    address public fooFightersAddress = address(0x9490165195503fcF6A0FD20aC113223fEfb66eD5);
+
+}
--- test/FooFighters.t.sol
+++ test/FooFighters.t.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.4;
+pragma solidity ^0.8.16;
 
 import { Cheats } from "forge-std/Cheats.sol";
 import { console } from "forge-std/console.sol";
 import { PRBTest } from "@prb/test/PRBTest.sol";
 
-/// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
-/// https://book.getfoundry.sh/forge/writing-tests
-contract ContractTest is PRBTest, Cheats {
+contract FooFightersTest is PRBTest, Cheats {
     function setUp() public {
         // solhint-disable-previous-line no-empty-blocks
     }
