0d180a79d046146b7abe03a733d7649e5b12065c
Joe Jordan <dev@ignitesoft.com>
2022-09-06 10:58:45 -0400

    WIP Fighters script

--- script/FooFighters.s.sol
+++ script/FooFighters.s.sol
@@ -2,14 +2,45 @@
 pragma solidity ^0.8.16;
 
 import { Script } from "forge-std/Script.sol";
-import { Foo } from "../src/FooFighters.sol";
+import { console } from "forge-std/console.sol";
+import { FooFighters } from "../src/FooFighters.sol";
 
-contract FooScript is Script {
-    Foo internal foo;
+contract FooFightersScript is Script {
+    FooFighters internal fooFighters = FooFighters(address(0x9490165195503fcF6A0FD20aC113223fEfb66eD5));
+    // ids: 20, 1109, 1187, 1195, 1199
+    uint[] myIds = [20, 1109, 1187, 1195, 1199];
 
     function run() public {
         vm.startBroadcast();
-        foo = new Foo();
+        findATarget();
         vm.stopBroadcast();
     }
+
+    function findATarget() public {
+        bool targetFound;
+        uint targetId;
+        uint randomId;
+        int power;
+
+        while (!targetFound) {
+            randomId = unsafeRandom(10148, 6669);
+            power = fooFighters.power(randomId);
+            console.log("ID:", randomId);
+            console.log("POWER:", uint(power));
+            console.log("HEALTH:", fooFighters.health(randomId));
+            if (randomId > 9500) {
+                targetFound = true;
+            }
+        }
+    }
+
+    function attackTarget(uint idToAttack) public {
+        // fooFighters.
+    }
+
+    /// @dev Returns a random value in [0, max-1]
+    function unsafeRandom(uint256 max, uint256 seed) internal view returns (uint256) {
+        return uint256(keccak256(abi.encode(blockhash(block.number - 1), block.timestamp, msg.sender, seed))) % max;
+    }
 }
+
--- test/FooFighters.t.sol
+++ test/FooFighters.t.sol
@@ -6,8 +6,10 @@ import { console } from "forge-std/console.sol";
 import { PRBTest } from "@prb/test/PRBTest.sol";
 
 contract FooFightersTest is PRBTest, Cheats {
+    uint256 public goerliFork;
+    address public playerAddress = address(0x5243d93bCdC8941Aa4b925c712128ba4933007C0);
     function setUp() public {
-        // solhint-disable-previous-line no-empty-blocks
+        goerliFork = vm.createSelectFork(vm.envString("ETH_GOERLI_RPC_URL"));
     }
 
     /// @dev Run Forge with `-vvvv` to see console logs.
