0975288a4abae866478438f7dc8d11fa01c0fd1d
Joe Jordan <dev@ignitesoft.com>
2022-09-06 00:28:35 -0400

    meh nevermind mint is closed already

--- src/FooFightersBot.sol
+++ src/FooFightersBot.sol
@@ -1,7 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-contract FooFightersBot {
+import { Owned } from "solmate/auth/Owned.sol";
+import { FooFighters } from "./FooFighters.sol";
+
+contract FooFightersBot is Owned {
+    FooFighters public fooFighters;
     address public fooFightersAddress = address(0x9490165195503fcF6A0FD20aC113223fEfb66eD5);
+
+    constructor() Owned(msg.sender) {
+        fooFighters = FooFighters(fooFightersAddress);
+    }
+
+    function mintFooFighters(uint16 _amount) external onlyOwner {
+        for (uint256 i = 0; i < _amount; ++i) {
+            fooFighters.FOOFIGHTER_mint();
+        }
+    }
 
 }
